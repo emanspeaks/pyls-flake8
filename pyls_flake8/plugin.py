ed152ad3463cd61c972f5f04e8179eef1454d1ab
Randy Eckman <randy.eckman@nasa.gov>
2018-11-09 12:39:00 -0600

    added simple tests and better severity logic

--- pyls_flake8/plugin.py
+++ pyls_flake8/plugin.py
@@ -1,20 +1,60 @@
 from pyls import hookimpl, lsp
 import re
 
+# default ignored by Flake8 package:
+# E121, continuation line under-indented for hanging indent
+# E123, closing bracket does not match indentation of opening bracket’s line
+# E126, continuation line over-indented for hanging indent
+# E226, missing whitespace around arithmetic operator
+# E241, multiple spaces after ‘,’
+# E242, tab after ‘,’
+# E704, multiple statements on one line (def)
+
 
 result_re = re.compile(r'stdin:(\d*):(\d*): (\w*) (.*)')
 
+# default flake messages to error except these
+flake_warnings = ('F401',  # module imported but unused
+                  'F402',  # import module from line N shadowed by
+                           #   loop variable
+                  'F403',  # ‘from module import *’ used;
+                           #   unable to detect undefined names
+                  'F404',  # future import(s) name after other statements
+                  'F405',  # name may be undefined, or defined from
+                           #   star imports: module
+                  'F602',  # dictionary key variable name repeated with
+                           #   different values
+                  )
+
+# default code style messages to warnings except these
+style_errors = ('E112',  # expected an indented block
+                'E113',  # unexpected indentation
+                'E741',  # do not use variables named ‘l’, ‘O’, or ‘I’
+                'E742',  # do not define classes named ‘l’, ‘O’, or ‘I’
+                'E743',  # do not define functions named ‘l’, ‘O’, or ‘I’
+                'E901',  # SyntaxError or IndentationError
+                'E902',  # IOError
+                )
+
 
 def results_to_diagnostic(results: str):
-    diags = list()
+    severity_enum = lsp.DiagnosticSeverity
+    warning = severity_enum.Warning
+    error = severity_enum.Error
 
+    diaglist = list()
     for line in results.splitlines():
         if line:
             linestr, col, code, msg = result_re.match(line).groups()
 
             lineno = int(linestr) - 1
             offset = int(col) - 1
-            severity = lsp.DiagnosticSeverity.Warning
+
+            if ((code[0] == 'F' and code not in flake_warnings)
+                    or code in style_errors):
+                severity = error
+            else:
+                severity = warning
 
             diag = {'source': 'flake8',
                     'range': {'start': {'line': lineno, 'character': offset},
@@ -24,24 +64,53 @@ def results_to_diagnostic(results: str):
                     'message': msg,
                     'severity': severity,
                     }
-            diags.append(diag)
+            diaglist.append(diag)
 
-    return diags
+    return diaglist
+
+
+def compile_flake8_args(config):
+    args = ['flake8']
+    for key, val in config.plugin_settings('flake8').items():
+        if key != 'enabled':
+            arg = '--' + key
+            if val and val is not True:
+                arg += '=' + str(val)
+            args.append(arg)
+    args.append('-')
+    return args
+
+
+def run_flake8(args, document):
+    from subprocess import run, PIPE
+    return run(args, stdout=PIPE, stderr=PIPE, input=document.source,
+               text=True)
 
 
 @hookimpl(tryfirst=True)
 def pyls_lint(config, document):
-    from subprocess import run, PIPE
-
-    args = ['flake8']
-    for key, val in config.plugin_settings('pyls_flake8').items():
-        if key != 'enabled':
-            arg = '--' + key
-            if val and val is not True:
-                arg += '=' + val
-            args.append('--' + key)
-    args.append('-')
-
-    p = run(args, stdout=PIPE, input=document.source, text=True)
-
+    args = compile_flake8_args(config)
+    p = run_flake8(args, document)
     return results_to_diagnostic(p.stdout)
+
+
+if __name__ == "__main__":
+    class test_document:
+        def __init__(self, src: str):
+            self.source = src
+
+    class test_config:
+        def plugin_settings(self, plugin: str) -> dict:
+            return {"extend-ignore": "W503"}
+
+    testdoc = test_document("import sys \nx.y()")
+    testcfg = test_config()
+
+    args = compile_flake8_args(testcfg)
+    print(args)
+    p = run_flake8(args, testdoc)
+    print('stderr: ' + p.stderr)
+    res = p.stdout
+    print(res)
+    diag = results_to_diagnostic(res)
+    print(diag)
